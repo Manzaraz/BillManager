ddf3d10ef32b5e4ee6886ed5406b84a8dcf6b186
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-07-25 22:43:18 -0300

    Step 5: Handle the Notification Response

--- BillManager/AppDelegate.swift
+++ BillManager/AppDelegate.swift
@@ -12,6 +12,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     private let remindActionID = "RemindAction"
     private let markAsPaidActionID = "MarkAsPaidAction"
     
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        
+        let id = response.notification.request.identifier
+        guard var bill = Database.shared.getBill(notificationID: id) else {
+            completionHandler()
+            return
+        }
+        
+        switch response.actionIdentifier {
+        case remindActionID:
+            let newRemindDate = Date().addingTimeInterval(60*60)
+            bill.scheduleReminder(on: newRemindDate) { (updatedBill) in
+                Database.shared.updateAndSave(updatedBill)
+            }
+        case markAsPaidActionID:
+            bill.paidDate = Date()
+            Database.shared.updateAndSave(bill)
+        default:
+            break
+        }
+        
+        completionHandler()
+        
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.list, .banner, .sound])
+    }
+    
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let remindAction = UNNotificationAction(identifier: remindActionID, title: "Recordarme después", options: [])
